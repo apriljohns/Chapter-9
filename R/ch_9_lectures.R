ca94264543de6f19c807e91d97d0c6ac913a409c
April Johns <april.johns@colostate.edu>
2025-11-12 08:56:37 -0700

    changed column to date class

--- R/ch_9_lectures.R
+++ R/ch_9_lectures.R
@@ -3,7 +3,8 @@ library(tidyverse)
 ufo_raw <- read_csv('data/ufo_sightings.csv')
 
 ufo_co <- ufo_raw %>% 
-  filter(state == 'co')
+  filter(state == 'co') %>% 
+  mutate(date_time = mdy_hm(date_time))
 
 
 
